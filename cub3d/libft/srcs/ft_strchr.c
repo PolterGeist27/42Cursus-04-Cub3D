d3bb6ce81d16d837ed4648b1ea27ea3d09b1c5b7
Diogo Tavares <diogo.martins.tavares27@gmail.com>
2023-06-05 10:02:03 +0100

    added libft

---
+++ cub3d/libft/srcs/ft_strchr.c
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strchr.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: pealexan <pealexan@student.42porto.com>    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/03 11:24:08 by pealexan          #+#    #+#             */
+/*   Updated: 2023/04/28 14:30:23 by pealexan         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../../includes/libft.h"
+
+char	*ft_strchr(const char *str, int c)
+{
+	int	i;
+
+	i = 0;
+	while (str[i] != (unsigned char)c)
+	{
+		if (str[i] == '\0')
+			return (0);
+		i++;
+	}
+	return (((char *)str) + i);
+}
