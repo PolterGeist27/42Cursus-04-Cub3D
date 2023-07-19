c287ba40f81188546014018a1ce217fa1d6124e0
Diogo Tavares <diogo.martins.tavares27@gmail.com>
2023-07-19 11:31:16 +0100

    Merged both branches

--- cub3d/includes/cub3d.h
+++ cub3d/includes/cub3d.h
@@ -6,7 +6,7 @@
 /*   By: diogmart <diogmart@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 11:59:40 by diogmart          #+#    #+#             */
-/*   Updated: 2023/06/20 16:10:40 by diogmart         ###   ########.fr       */
+/*   Updated: 2023/07/19 11:27:50 by diogmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 
 #include "libft.h"
 #include "../minilibx-linux/mlx.h"
+#include <X11/X.h>
 #include <math.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -42,10 +43,17 @@ typedef struct s_data {
 }	t_data;
 
 //      KEY VALUES:     //
-#define ESC 65307
+# define ESC 65307
+# define W 119
+# define A 97
+# define S 115
+# define D 100
+# define LEFT 65361
+# define RIGHT 65363
 
 //      init.c          //
 void	init_data(t_data *data);
+void	file_type(char *file);
 int	    ft_close(t_data *data);
 
 //      keys.c          //
--- cub3d/srcs/main.c
+++ cub3d/srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: diogmart <diogmart@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/20 11:48:36 by diogmart          #+#    #+#             */
-/*   Updated: 2023/06/20 16:05:49 by diogmart         ###   ########.fr       */
+/*   Updated: 2023/07/19 11:28:35 by diogmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,10 +18,16 @@ int main(int argc, char **argv)
     (void)argc;
     (void)argv;
 
+	if (argc != 2)
+	{
+		ft_putstr_fd("Wrong number of arguments\n", 2);
+		exit(2);
+	}
+	file_type(argv[1]);
     data = (t_data *)malloc(sizeof(t_data));
-    init_data(data);
-    mlx_hook(data->mlx_win, 2, (1L << 0), check_keys, data);
+	init_data(data);
     mlx_hook(data->mlx_win, 17, 0, ft_close, data);
+    mlx_hook(data->mlx_win, KeyPress, KeyPressMask, check_keys, data);
     mlx_loop(data->mlx);
     //ft_close(data);
     return (0);
