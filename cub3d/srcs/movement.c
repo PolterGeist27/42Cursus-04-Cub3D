e7da290f76259edabf4b71f835fa3008ddc54d2c
Diogo Tavares <diogo.martins.tavares27@gmail.com>
2023-09-20 12:11:27 +0100

    cleared up the code a bit

--- cub3d/srcs/movement.c
+++ cub3d/srcs/movement.c
@@ -6,7 +6,7 @@
 /*   By: diogmart <diogmart@student.42porto.com>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/09/19 11:15:03 by diogmart          #+#    #+#             */
-/*   Updated: 2023/09/20 11:56:46 by diogmart         ###   ########.fr       */
+/*   Updated: 2023/09/20 12:08:26 by diogmart         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,42 +16,42 @@ void    move_vertical(t_data *data, t_player *player, int dir)
 {
     if (dir == 1)
     {
-        if (data->map[(int)player->pos_y]
-                [(int)(player->pos_x + player->dir_x * 0.2)] == '0')
-            player->pos_x += player->dir_x * 0.2;
         if (data->map[(int)(player->pos_y + player->dir_y * 0.2)]
-                [(int)player->pos_x] == '0')
-            player->pos_y += player->dir_y * 0.2;
-    }
+                [(int)(player->pos_x + player->dir_x * 0.2)] == '0')
+		{
+        	player->pos_x += player->dir_x * 0.2;
+        	player->pos_y += player->dir_y * 0.2;
+		}
+	}
     else
     {
-        if (data->map[(int)player->pos_y]
-                [(int)(player->pos_x - player->dir_x * 0.2)] == '0')
-            player->pos_x -= player->dir_x * 0.2;
         if (data->map[(int)(player->pos_y - player->dir_y * 0.2)]
-                [(int)player->pos_x] == '0')
+                [(int)(player->pos_x - player->dir_x * 0.2)] == '0')
+		{
+            player->pos_x -= player->dir_x * 0.2;
             player->pos_y -= player->dir_y * 0.2;
-    }
+		}
+	}
 }
 
 void    move_horizontal(t_data *data, t_player *player, int dir)
 {
 	if (dir == 1)
     {
-        if (data->map[(int)player->pos_y]
-                [(int)(player->pos_x - player->dir_y * 0.2)] == '0')
-            player->pos_x -= player->dir_y * 0.2;
         if (data->map[(int)(player->pos_y - player->dir_x * 0.2)]
-                [(int)player->pos_x] == '0')
+                [(int)(player->pos_x - player->dir_y * 0.2)] == '0')
+		{
+            player->pos_x -= player->dir_y * 0.2;
             player->pos_y -= player->dir_x * 0.2;
-    }
+		}
+	}
     else
     {
-        if (data->map[(int)player->pos_y]
-                [(int)(player->pos_x + player->dir_y * 0.2)] == '0')
-            player->pos_x += player->dir_y * 0.2;
         if (data->map[(int)(player->pos_y + player->dir_x * 0.2)]
-                [(int)player->pos_x] == '0')
+                [(int)(player->pos_x + player->dir_y * 0.2)] == '0')
+		{
+            player->pos_x += player->dir_y * 0.2;
             player->pos_y += player->dir_x * 0.2;
-    }
+		}
+	}
 }
